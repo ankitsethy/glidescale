0f12c6048eca80a47a2e6ace6ddfa87f9ae62534
ankitsethy4444@gmail.com <ankitsethy4444@gmail.com>
2026-03-01 00:32:06 +0530

    fix(logo): strip dark background for transparency & adjust navbar/footer sizes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer: React.FC = () => {
 
           <div className="flex items-center justify-center md:justify-start">
             <a href="/" className="flex items-center group transition-opacity hover:opacity-80">
-              <img src="/logo-dark.svg" alt="Glidescale" className="h-[26px] w-auto object-contain" />
+              <img src="/logo-dark.svg" alt="Glidescale" className="h-[34px] w-auto object-contain" />
             </a>
           </div>
 
